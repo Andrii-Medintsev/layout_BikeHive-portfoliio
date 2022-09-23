9f51115e22a1fbd56c959071d722546f6217bd70
Andrii Medintsev <medintsev.andrew@gmail.com>
2022-09-23 22:29:49 +0300

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', noReload);
+
+function noReload(event) {
+  event.preventDefault();
+
+  form.reset();
+}
